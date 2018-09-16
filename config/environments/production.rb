b3021f9f91c095e4d448787abc8ef615d8bbe7cd
Daniel Ball <g.thirteen333@gmail.com>
2018-09-16 05:44:22 +0000

    fixed app in production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'afternoon-spire-94945.herokuapp.com'
+  host = 'enigmatic-beach-36684.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
