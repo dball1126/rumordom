39c277e606e1c926514a3795c90aa40eed8868f7
Daniel Ball <g.thirteen333@gmail.com>
2018-10-25 02:58:55 +0000

    Fixed Gecoding

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,3 +1,29 @@
 Geocoder.configure(
-  timeout: 10
+    #changed 10 24 18   trying to fix geocoder. ITS WORKS !!!!!
+  :timeout=>20,
+
+  :lookup=>:yandex,
+
+  :ip_lookup=>:telize,
+
+  :language=>:en,
+
+  :http_headers=>{},
+
+  :use_https=>false,
+
+  :http_proxy=>nil,
+
+  :https_proxy=>nil,
+
+  :api_key=>nil,
+
+  :cache=>nil,
+
+  :cache_prefix=>"geocoder:",
+
+  :units=>:km,
+
+  :distances=>:linear
+
 )
