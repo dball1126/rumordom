0dc4de54892ce85adc93cc85dddf5ebd6b54c329
Daniel Ball <g.thirteen333@gmail.com>
2018-11-16 04:17:56 +0000

    Fixed password reset
    Former-commit-id: 6a23317870e75a440c18b12d3ced3af0ef9f2e33

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -38,6 +38,10 @@ class PasswordResetsController < ApplicationController
   
   private
 
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
     def get_user
       @user = User.find_by(email: params[:email])
     end
