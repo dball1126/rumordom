16ea7567087706509947cc94948690ff95a21b69
Daniel Ball <g.thirteen333@gmail.com>
2018-12-14 01:11:49 +0000

    removing previous additions
    Former-commit-id: c3502331c0e9b63a02bdee0f9561b851e365e2a5

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,10 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-module New_foodback
+module Foodback
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
