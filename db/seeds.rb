e431c55e0ca7f187c9cc3651e84c653cc46122c1
Daniel Ball <g.thirteen333@gmail.com>
2019-01-03 23:02:41 +0000

    import ready
    Former-commit-id: fd2f529efde150737c00e81c903bd58524c28606

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'nyp2100k.csv'))
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'ny1100k.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
   busz = Business.new
