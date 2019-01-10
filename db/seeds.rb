4f7c2b771a840c8acdb34fc8e8b9e0867544cd07
Daniel Ball <g.thirteen333@gmail.com>
2019-01-10 00:10:27 +0000

    scripts not run yet, fourth ny2 100k batch
    Former-commit-id: 0f3ddd7667168017f9bee634f9a36745f1dcb7d5

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'ny2300k.csv'))
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'ny2400k.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
   busz = Business.new
