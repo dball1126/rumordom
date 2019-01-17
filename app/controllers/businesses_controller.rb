3577f839cb446970440a00aaf88de112899b2b78
Daniel Ball <g.thirteen333@gmail.com>
2019-01-17 23:10:27 +0000

    uncommented Business.within in controller
    Former-commit-id: d3b7d84bd0e0fa1b79e97632327a73abdcaaa4fa

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -84,11 +84,12 @@ class BusinessesController < ApplicationController
     #@businessz  = Business.search(params)
     #@businesses = Business.search(params)
     @businesses = Business.search(params).paginate(:page => params[:page], :per_page => 30)
-   # Business.within(
-  #  params[:radius],
-  #  :units => :miles,
-  #  :origin => [params[:lat], params[:lng]]
-  #)
+    
+    Business.within(
+    params[:radius],
+    :units => :miles,
+    :origin => [params[:lat], params[:lng]])
+    
     load_businessz
   end
   
